612009197b62e2a2f939324a9170510cce35b0d0
srkhan <sadadk673@gmail.com>
2017-04-18 09:52:42 -0400

    Update main.C

--- main.C
+++ main.C
@@ -1,3 +1,31 @@
-//ar
+#include <functional> 
+#include <queue> 
+#include <vector> 
+#include <iostream>
 
-//tester edit
+
+using namespace std;
+priority_queue<Item*, vector<Item*>, compareItem> q;
+while(!q.empty()) {
+        item = q.top();
+        //cout << item << endl;
+        cout << "Time: " << item -> getKey() << " : ";
+        item -> run();
+        q.pop();
+    }
+
+int main()
+{ 
+  //this part we might need to change idk yet
+  int runtime[10000];
+  for(int i = 0; i<=10001; i++)
+  {
+  	runtime[i]=i;
+  }
+  
+  std::priority_queue<int, std::vector<int>, std::greater<int> > q; 
+  for(int i = 0; i < 10001; i++) q.push(runtime[i]);
+ 
+  print_queue(q); 
+}
+

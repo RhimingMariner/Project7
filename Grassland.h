fd82daf5fedaba9fda15b6dd226ecb4b58495a4f
Robert Ditmore <robertsditmore@gmail.com>
2017-04-18 13:28:10 -0400

    Update Grassland.h

--- Grassland.h
+++ Grassland.h
@@ -1,15 +1,19 @@
 //ar
 
 
-
+#ifndef GRASSLAND_H
+#define GRASSLAND_H
 #include "Item.h"
-#ifndef
-#define
+#include <iostream>
+#include<stdlib.h>
+#include <queue>
+
 include namespace std;
 
 class Grassland : public Item{
   priority_queue<Item*, vector<Item*>, compareItem>* q;
   public:
+    Grassland();
     Grassland(int k, priority_queue<Item*, vector<Item*>, compareItem >* q1) : item(k), q(q1){}
     void Run(){}
     ~Grassland(){}

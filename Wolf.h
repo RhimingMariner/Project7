70a16174b893b0b0f62169782614cfd772df7a22
srkhan <sadadk673@gmail.com>
2017-04-18 13:24:26 -0400

    Update Wolf.h

--- Wolf.h
+++ Wolf.h
@@ -1,1 +1,28 @@
+#ifndef WOLF_H
+#define WOLF_H
+#include "Animal.h"
+#include "Grassland.h"
+#include <iostream>
+#include <list>
+#include <stdlib.h>
+#include <queue>
 
+using namespace std;
+
+
+class Wolf : public Item{
+  priority_queue<Item*, vector<Item*>, compareItem>* q;
+  public:
+    Wolf();
+    Wolf(int k, priority_queue<Item*, vector<Item*>, compareItem >* q1) : item(k), q(q1){}
+    void Run(){};
+    ~Wolf(){};
+    //void Run(){
+    //do something 
+    }
+  
+  
+    //~Rabbit(){}
+};
+
+#endif

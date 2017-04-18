acbdedf5aa3f2821829ae6da18c1821efe063b08
Robert Ditmore <robertsditmore@gmail.com>
2017-04-17 23:03:07 -0400

    Update Item.h

--- Item.h
+++ Item.h
@@ -3,17 +3,25 @@
 /*
 class Item{
   int current;
-  int Key;
+  int key;
   public:
-    Item(int k) : Key(k){}
-    int getKey() {return Key;}
-    void setKey(int k){Key = k;}
+  Item() : key(0) {}
+    Item(int k) : key(k){}
+    int getKey() {return key;}
+    void setKey(int k){key = k;}
     friend class Grasslands;
-    virtual void Run() {}
+    friend class Animal;
+    friend class LazyRabbit;
+    friend class ActiveRabbit;
+    friend class Wolf;
+    friend class Rabbit;
+    virtual void run() {cout << "Ran Item. NOT OK" << endl;}
     virtual ~Item(){}
 
 };
 
+
+//move to animal
 //static Kill(Item I){
 //  delete->this;
 //}

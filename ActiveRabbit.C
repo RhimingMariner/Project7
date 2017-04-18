e54ce9a18008d51f5d5bb11331e3b7abd6ca902a
srkhan <sadadk673@gmail.com>
2017-04-18 13:33:46 -0400

    Update ActiveRabbit.C

--- ActiveRabbit.C
+++ ActiveRabbit.C
@@ -1,1 +1,27 @@
-//ar
+
+#include "Item.h"
+#include "Grassland.h"
+#in
+#include <iostream>
+#include <stdlib.h>
+#include <list>
+#include <queue>
+
+using namespace std;
+
+class ActiveRabbit: public Item{
+  priority_queue<Item*, vector<Item*>, compareItem>* q;
+  public:
+    ActiveRabbit::ActiveRabbit()
+    {
+      
+    };
+    ActiveRabbit(int k, priority_queue<Item*, vector<Item*>, compareItem >* q1) : item(k), q(q1){}
+    void Run(){cout << "Animal at " << getkey() << endl;
+                setKey(getKey()+2);
+                q->push(this);}
+    ActiveRabbit::~ActiveRabbit()
+    {
+      
+    }
+};

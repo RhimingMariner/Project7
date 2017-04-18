9747c210ee9a7798c4563194cbcc7096f679e792
srkhan <sadadk673@gmail.com>
2017-04-18 13:27:36 -0400

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -18,9 +18,9 @@ Class Animal : public Item{
     int wolf;
     int lazyRabbit;
     //int activeRabbit;
-    Animal(int k, priority_queue<Item*, vector<Item*>, compareItem >* q1) : item(k), q(q1){}
-    void Run(){}
-    ~Animal(){}
+    Animal(int k, priority_queue<Item*, vector<Item*>, compareItem >* q1) : item(k), q(q1){};
+    void Run(){};
+    ~Animal(){};
 };
   
 #endif

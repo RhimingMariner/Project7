e3bd4eb728b98bf38f6dd07015e5225efe47bd8f
Robert Ditmore <robertsditmore@gmail.com>
2017-04-18 12:49:41 -0400

    Update LazyRabbit.C

--- LazyRabbit.C
+++ LazyRabbit.C
@@ -1,1 +1,24 @@
-//ar
+
+#include "Item.h"
+#ifndef
+#define
+using namespace std;
+
+Class LazyRabbit : public Item{
+  priority_queue<Item*, vector<Item*>, compareItem>* q;
+  public:
+    LazyRabbit(int k, priority_queue<Item*, vector<Item*>, compareItem >* q1) : item(k), q(q1){}
+    void Run(){cout << "LazyRabbit at " << getKey() << endl;
+                setKey(getKey()+1;
+                q->push(this);}
+    ~LazyRabbit(){}
+};
+  
+  
+  
+  
+  
+  
+  
+  
+#endif

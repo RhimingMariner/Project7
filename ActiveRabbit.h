7bd4eb3254e288f23fa127aba930c77108536848
Robert Ditmore <robertsditmore@gmail.com>
2017-04-18 13:17:02 -0400

    Update ActiveRabbit.h

--- ActiveRabbit.h
+++ ActiveRabbit.h
@@ -1,1 +1,22 @@
 //ar
+
+#ifndef ACTIVERABBIT_H
+#define ACTIVERABBIT_H
+#include "Rabbit.h"
+#include "Grassland.h"
+#include <iostream>
+#include <queue>
+#include <list>
+#include <stdlib.h>
+
+using namespace std;
+
+Class ActiveRabbit : public Item{
+  priority_queue<Item*, vector<item*>, compareItem>* q;
+  public:
+    ActiveRabbit(int k, priority_queue<Item*, vector<Item*>, compareItem >* q1) : item(k), q(q1){}
+    void Run(){}
+    ~ActiveRabbit(){}
+
+};
+#endif

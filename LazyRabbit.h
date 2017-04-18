e09bdd46788635f2f045763bb6fd6483675dbc14
Robert Ditmore <robertsditmore@gmail.com>
2017-04-18 11:41:07 -0400

    Update LazyRabbit.h

--- LazyRabbit.h
+++ LazyRabbit.h
@@ -1,1 +1,32 @@
 //ar
+//extern priority_queue<Item*, vector<Item*>, compareItem> pq;
+//extern Grass grassGrid[512][512] gg;
+
+
+/*
+void LazyRabbit::run(){
+  //do stuff (check if it died)
+  //3 checks, isAlive(), Starvation(), 
+  if(this->isAlive())
+  {
+    if(!(this->Starvation()))//change to Current - LastEaten >= 5
+    {
+      if(!(this->tooOld()))//change to 
+      {
+        
+      }
+    }
+  }
+  else
+  {
+    //increment DeadLRPop
+    //decrement CurrentLRPop
+    
+  }
+  //check if there is food (double for)
+  key += 1;
+  pq.push(this);
+}
+
+
+*/

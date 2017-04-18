b7ed9ba3be9540d2eb1bd96379c88b0c8658c3a4
Robert Ditmore <robertsditmore@gmail.com>
2017-04-18 13:21:01 -0400

    Update ActiveRabbit.h

--- ActiveRabbit.h
+++ ActiveRabbit.h
@@ -14,6 +14,7 @@ using namespace std;
 class ActiveRabbit : public Item{
   priority_queue<Item*, vector<item*>, compareItem>* q;
   public:
+    ActiveRabbit();
     ActiveRabbit(int k, priority_queue<Item*, vector<Item*>, compareItem >* q1) : item(k), q(q1){}
     void Run(){}
     ~ActiveRabbit(){}

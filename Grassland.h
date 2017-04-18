682257d8866ee9e79280640c498f40e6785309c8
Robert Ditmore <robertsditmore@gmail.com>
2017-04-18 13:26:29 -0400

    Update Grassland.h

--- Grassland.h
+++ Grassland.h
@@ -7,7 +7,7 @@
 #define
 include namespace std;
 
-Class Grassland : public Item{
+class Grassland : public Item{
   priority_queue<Item*, vector<Item*>, compareItem>* q;
   public:
     Grassland(int k, priority_queue<Item*, vector<Item*>, compareItem >* q1) : item(k), q(q1){}

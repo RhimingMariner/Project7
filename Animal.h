c1a91980f46632b33e2f04544f5d789bf3764a44
srkhan <sadadk673@gmail.com>
2017-04-19 11:42:38 -0400

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -1,26 +1,24 @@
+
 #ifndef ANIMAL_H
 #define ANIMAL_H
 #include "Item.h"
-#include "Grassland.h"
-#include <iostream>
-#include <stdlib.h>
-#include <queue>
-
 
 using namespace std;
 
 class Animal : public Item{
   
-  priority_queue<Item*, vector<Item*>, compareItem>* q;
+  protected:
+    int x, y;
+    
+ 
   public:
-    Animal();
-    Animal(constructor);
-    int wolf;
-    int lazyRabbit;
-    //int activeRabbit;
-    Animal(int k, priority_queue<Item*, vector<Item*>, compareItem >* q1) : item(k), q(q1){};
-    void Run(){};
-    ~Animal(){};
+     priority_queue<Item*, vector<Item*>, compareItem>* q;
+    Animal(int key, priority_queue<Item*, vector<Item*>, compareItem>* q1) : Item(key), q(q1){}
+    
+    virtual void Run(){}
+
+    virtual ~Animal(){}
+
 };
   
 #endif

741b91ae70f7509b22f4b858397594afd2016336
srkhan <sadadk673@gmail.com>
2017-04-19 11:43:23 -0400

    Update Item.h

--- Item.h
+++ Item.h
@@ -1,8 +1,5 @@
-//ar
-
-/*
-#ifndef ITEM_H_
-#define ITEM_H_
+#ifndef ITEM_H
+#define ITEM_H
 #include <iostream>
 #include <queue>
 #include <vector>
@@ -12,10 +9,10 @@ class Item{
   public:
     int current;
     int key;
-    //Item() : key(0) {}
     Item(int k) : key(k){}
     int getKey() {return key;}
     void setKey(int x){key = x;}
+    Item(){}
     friend class compareItem;
     friend class Grasslands;
     friend class Animal;
@@ -23,23 +20,19 @@ class Item{
     friend class ActiveRabbit;
     friend class Wolf;
     friend class Rabbit;
-    virtual void run() {cout << "Ran Item. NOT OK" << endl;}
+    virtual void Run() {}
     virtual ~Item(){}
 
 };
 
-
-//move to animal
-//static Kill(Item I){
-//  delete->this;
-//}
-
 struct compareItem
-: binary_function<Item*, Item*, bool>
 {
   bool operator()(const Item* t1, const Item* t2) const
   {
     return (t1->key > t2->key);
   }
-}
-*/
+};
+
+#endif
+
+

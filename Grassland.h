53bfddf246b14189f85a21593cd458e20e52f8eb
srkhan <sadadk673@gmail.com>
2017-04-19 17:56:42 -0400

    Update Grassland.h

--- Grassland.h
+++ Grassland.h
@@ -1,49 +1,16 @@
-
-/*
 #ifndef GRASSLAND_H
 #define GRASSLAND_H
 #include "Item.h"
 #include "Animal.h"
-#include <iostream>
-#include <stdlib.h>
-#include <queue>
+
 
 using namespace std;
 
-class Cell {
-	Animal* Animal;
-	int lastEaten;
-	int x;
-	int y;
+
+class Cell : public Item{
+
 };
-//forward declare animal, wolf, rabbit
 class Grassland:public Item{
-  priority_queue<Item*, vector<Item*>, compareItem>* q;
-
-  public:
-  	//FindRabbitAround(int x, int y);
-  	Grassland(int lr, int ar, int w);
-  	void Report() //reports scores
-  	virtual void Run() // virtual function
- 	Rabbit* FindRabbitAround(int x, int y);
- 	void MoveWolf(int x, int y);
- 	void MoveRabbit(int x, int y);
-  	virtual ~Grassland(); //delete something
-  	/*
-    Grassland();
-    Grassland(int k, priority_queue<Item*, vector<Item*>, compareItem >* q1) : item(k), q(q1){}
-    void Run(){}
-    ~Grassland(){}
-    //put end comment here
+public:
 };
-
-  
-  
-  
-  
-  
-  
-  
-  
 #endif
-*/
